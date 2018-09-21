a5fe19e8b089578c88f914720167cbfd4bd1ff7c
Me <gh.2018.08.26@outlook.com>
2018-09-21 12:11:19 -0500

    auto

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,3 @@
-function Person(fullName, favColor) {
-   this.name = fullName;
-   this.favoriteColor = favColor;
-   this.greet = function() {
-      console.log("Hello, my name is " + this.name + ", and my favorite color is " + this.favoriteColor +".");
-   }
-}
-
 var john = new Person("John Doe", "blue");
 john.greet();
 
