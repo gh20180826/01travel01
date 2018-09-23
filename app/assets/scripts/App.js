04f44c0251220fcc04ed31cb277dc5feffdbb97a
Me <gh.2018.08.26@outlook.com>
2018-09-23 11:56:37 -0500

    first vid finished about fading in on scroll (vid 44)

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -9,6 +9,9 @@ john.greet();
 var jane = new Person("Jane Smith", "orange");
 jane.greet();
 */
+
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
 
 var mobileMenu = new MobileMenu();
+var revealOnScroll = new RevealOnScroll();
