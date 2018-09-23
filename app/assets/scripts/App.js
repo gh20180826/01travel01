fab838a013f024a494e55faebdaca913731d83b5
Me <gh.2018.08.26@outlook.com>
2018-09-23 17:02:09 -0500

    finished Reveal Elements on Scroll vids (end vid 45)

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -12,6 +12,8 @@ jane.greet();
 
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
-var revealOnScroll = new RevealOnScroll();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
