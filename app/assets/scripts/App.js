c513bcbdc71388225a3f430f781e852d1317d704
Me <gh.2018.08.26@outlook.com>
2018-09-24 13:41:50 -0500

    making (not finished) sticky header (vid 46)

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -10,10 +10,18 @@ var jane = new Person("Jane Smith", "orange");
 jane.greet();
 */
 
+
+
+
+// below (in color) is active:
+
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 import $ from 'jquery';
+
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
+var stickyHeader = new StickyHeader();
