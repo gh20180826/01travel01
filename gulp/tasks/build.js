f0d20b3bee0e6d852a2f813feeb2a8aca92eb543
Me <gh.2018.08.26@outlook.com>
2018-09-26 17:38:21 -0500

    auto

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 del = require('del'),
-usemin = require('gulp-usemin');
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify');
 
 
 
@@ -25,8 +28,8 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
 gulp.task('usemin', ['deleteDistFolder'], function() {
    return gulp.src("./app/index.html")
       .pipe(usemin({
-         css: [],
-         js: []
+         css: [function() {return rev()}, function() {return cssnano()}],
+         js: [function() {return rev()}, function() {return uglify()}]
       }))
       .pipe(gulp.dest("./dist"));
 });
