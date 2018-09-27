14d663a72514730df1c34014cdb140f5e57710b8
Me <gh.2018.08.26@outlook.com>
2018-09-27 11:44:18 -0500

    gulp build is working as of 18.04min into vid 56

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,11 +1,7 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 del = require('del'),
-usemin = require('gulp-usemin'),
-rev = require('gulp-rev'),
-cssnano = require('gulp-cssnano'),
-uglify = require('gulp-uglify');
-
+usemin = require('gulp-usemin');
 
 
 
@@ -27,10 +23,7 @@ gulp.task('optimizeImages', ['deleteDistFolder'], function() {
 
 gulp.task('usemin', ['deleteDistFolder'], function() {
    return gulp.src("./app/index.html")
-      .pipe(usemin({
-         css: [function() {return rev()}, function() {return cssnano()}],
-         js: [function() {return rev()}, function() {return uglify()}]
-      }))
+      .pipe(usemin())
       .pipe(gulp.dest("./dist"));
 });
 
