2635eb26834e0274fa94513ab00620f00f0b840e
Me <gh.2018.08.26@outlook.com>
2018-10-01 12:39:15 -0500

    auto

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -18,7 +18,6 @@ jane.greet();
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
-import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal';
 
 
