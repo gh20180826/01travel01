9aeb1edcc27225e583140961d7da6d7160e23af9
Me <gh.2018.08.26@outlook.com>
2018-10-01 12:42:24 -0500

    auto

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -16,14 +16,10 @@ jane.greet();
 // below (in color) is active:
 
 import MobileMenu from './modules/MobileMenu';
-import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import Modal from './modules/Modal';
 
 
 
 var mobileMenu = new MobileMenu();
-new RevealOnScroll($(".feature-item"), "85%");
-new RevealOnScroll($(".testimonial"), "60%");
-var stickyHeader = new StickyHeader();
 var modal = new Modal();
